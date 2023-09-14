e0aafe4d5a25af4c23ae865992b9fa3f9fd6780b
Petar Radojicic <petar@radojicic.co>
2023-09-14 15:07:55 +0200

    AddedToken

--- src/modules/API.tsx
+++ src/modules/API.tsx
@@ -1,9 +1,10 @@
 import axios from "axios";
+let token = sessionStorage.getItem("token");
 
 export const submitInterview = (APICALL: any, body?: any) => {
   return axios.post(`http://localhost:3333/api/${APICALL}`, body, {
     headers: {
-      Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6I.jEifQ.T1gAxZhcDJ49PUD9jnv_7P_SqfC60B6aTCo0KlE5Cys`
+      Authorization: token
     }
   });
 }
@@ -11,15 +12,16 @@ export const submitInterview = (APICALL: any, body?: any) => {
 export const getInterview = (APICALL: any) => {
   return axios.get(`http://localhost:3333/api/${APICALL}`, {
     headers: {
-      Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6I.jEifQ.T1gAxZhcDJ49PUD9jnv_7P_SqfC60B6aTCo0KlE5Cys`
+      Authorization: token
     }
   });
 }
 
 export const deleteInterview = (APICALL: any,ID: any) => {
+  console.log(token)
   return axios.delete(`http://localhost:3333/api/${APICALL}/${ID}`, {
     headers: {
-      Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6I.jEifQ.T1gAxZhcDJ49PUD9jnv_7P_SqfC60B6aTCo0KlE5Cys`
+      Authorization: token
     }
   });
 }
--- src/modules/loginAuth.ts
+++ src/modules/loginAuth.ts
@@ -1,6 +1,21 @@
+import {postInterview} from '../modules/API'
+
 export const loginAuth = (username: string, password: string) => {
 
-    if (username && password == 'admin') {
+    const payload = {
+        "email": "dev@dev.com",
+        "password": "developer"
+      }
+
+
+    const getToken = async () => {
+        postInterview(payload).then(response => {
+            sessionStorage.setItem("token",response.data.accessToken);
+          })
+    }
+    getToken()
+
+    if (username && password == 'root') {
         return "AdminPanel"
     }
     else {
