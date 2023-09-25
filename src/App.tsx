03e3522594f47f0d9a095942de60dadd793d642d
Petar Radojicic <petar@radojicic.co>
2023-09-25 12:20:27 +0200

    router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import { Router } from "./utils/Router";
+import { Footer } from "../src/components/Footer/Footer"
+import { Header } from "../src/components/Header/Header"
 
 export const App = () => {
 
   return (
-    <Router />
-
+    <>
+      <Header />
+      <Router />
+      <Footer />
+    </>
   );
 }
--- src/utils/Router.tsx
+++ src/utils/Router.tsx
@@ -4,14 +4,10 @@ import { Login } from "../pages/Login/Login";
 import { UsersPanel } from "../pages/UsersPanel/UsersPanel";
 import { UserPanel } from "../pages/UserPanel/UserPanel";
 import { AdminPanel } from "../pages/AdminPanel/AdminPanel";
-import { Footer} from "../components/Footer/Footer"
-import { Header} from "../components/Header/Header"
 
 export const Router = () => {
 
     return <>
-    {/* Header and Footer components should be in app given how they have nothing to do with routing  */}
-        <Header /> 
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Login />} />
@@ -20,6 +16,5 @@ export const Router = () => {
                 <Route path="/AdminPanel" element={<AdminPanel/>} />
             </Routes>
         </BrowserRouter>
-        <Footer />
     </>
 }
